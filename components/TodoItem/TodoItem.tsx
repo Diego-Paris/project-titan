7b62a6cc974cbf2051372bd8ea53cf27109f1c73
Diego-Paris <diego.paris@upr.edu>
2024-01-09 13:36:46 -0400

    added changes for notifications

--- components/TodoItem/TodoItem.tsx
+++ components/TodoItem/TodoItem.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import React, { useState } from 'react';
-import { Checkbox, Text, Group, Paper, Box, Alert } from '@mantine/core';
+import { Checkbox, Text, Group, Paper, Box, useMantineTheme } from '@mantine/core';
+import { notifications } from '@mantine/notifications';
+import { IconCheck, IconX } from '@tabler/icons-react';
 import { Todo } from '@prisma/client';
 import { updateTodoAction } from '@/app/_actions';
 
@@ -11,25 +13,53 @@ type TodoItemProps = {
 
 const TodoItem = ({ todo }: TodoItemProps) => {
   const [isCompleted, setIsCompleted] = useState(todo.isCompleted);
-  const [error, setError] = useState('');
+  const theme = useMantineTheme();
 
   const handleCheckboxChange = async (checked: boolean) => {
     // Optimistically update the UI
     setIsCompleted(checked);
 
+    const notificationId = `todo-update-${todo.id}`;
+
+    // Show loading notification
+    notifications.show({
+      id: notificationId,
+      loading: true,
+      title: 'Updating Todo',
+      message: 'Please wait...',
+      autoClose: false,
+      withCloseButton: false,
+    });
+
     try {
       // Send the update to the server
       await updateTodoAction(todo.id, checked);
+      notifications.update({
+        id: notificationId,
+        color: 'teal',
+        title: 'Todo Updated',
+        message: 'Todo has been updated successfully.',
+        icon: <IconCheck size={theme.fontSizes.md} />,
+        loading: false,
+        autoClose: 2000,
+      });
     } catch (err) {
       // On error, revert the UI change and show an error message
       setIsCompleted(!checked);
-      setError('Failed to update todo. Please try again.');
+      notifications.update({
+        id: notificationId,
+        color: 'red',
+        title: 'Failed to Update Todo',
+        message: 'An error occurred. Please try again.',
+        icon: <IconX size={theme.fontSizes.md} />,
+        loading: false,
+        autoClose: 2000,
+      });
     }
   };
 
   return (
     <Paper withBorder shadow="xs" p="sm" radius="md" my="xs">
-      {error && <Alert color="red">{error}</Alert>}
       <Group wrap="nowrap">
         <Checkbox
           id={todo.id}
@@ -51,7 +81,7 @@ const TodoItem = ({ todo }: TodoItemProps) => {
             {todo.title}
           </Text>
         </Box>
-        <Text size="sm" color="dimmed">
+        <Text size="sm" c="dimmed">
           {todo.updatedAt.toUTCString()}
         </Text>
       </Group>
