9c85022c573259c9e2132028cbd5653f7aee34c4
Diego-Paris <diego.paris@upr.edu>
2024-01-12 00:47:39 -0400

    testing no cache

--- app/_actions.ts
+++ app/_actions.ts
@@ -4,9 +4,9 @@ import { revalidatePath, unstable_noStore as noStore } from 'next/cache';
 import { createTodo, deleteTodo, getTodos, updateTodo } from '@/lib/todos';
 
 export async function getTodosAction() {
-  noStore();
-  const { todos } = await getTodos();
   revalidatePath('/');
+  //noStore();
+  const { todos } = await getTodos();
   return { todos };
 }
 
