58e30cbc8f5d5f6463c1ca45ad22bd9f6b9a0e4f
Diego-Paris <diego.paris@upr.edu>
2024-01-14 04:13:27 -0400

    added react query dev tools

--- lib/providers/tanstack-query.tsx
+++ lib/providers/tanstack-query.tsx
@@ -2,7 +2,7 @@
 
 import type { PropsWithChildren, ReactElement } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { useState } from 'react';
 
@@ -18,5 +18,10 @@ export function TanStackQuery({ children }: PropsWithChildren): ReactElement {
       })
   );
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
