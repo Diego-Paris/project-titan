b550aa6067b774438a6ad71fd4854cf5ff1840e6
Diego-Paris <diego.paris@upr.edu>
2024-01-20 20:57:12 -0400

    added header

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { HeaderMegaMenu } from '@/components/HeaderMegaMenu/HeaderMegaMenu';
 
 export default function HomeLayout({ children }: { children: any }) {
-  return <>{children}</>;
+  return (
+    <>
+      <HeaderMegaMenu />
+      {children}
+    </>
+  );
 }
