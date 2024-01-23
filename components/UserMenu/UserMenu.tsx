ddcf49612fb288637e85ccc057cdb6044aa0936e
Diego-Paris <diego.paris@upr.edu>
2024-01-23 00:46:16 -0400

    refined navbar

--- components/UserMenu/UserMenu.tsx
+++ components/UserMenu/UserMenu.tsx
@@ -1,30 +1,19 @@
 import cx from 'clsx';
 import { useState } from 'react';
-import {
-  Container,
-  Avatar,
-  UnstyledButton,
-  Group,
-  Text,
-  Menu,
-  Tabs,
-  Burger,
-  rem,
-  useMantineTheme,
-} from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+import { Avatar, UnstyledButton, Group, Text, Menu, rem, useMantineTheme } from '@mantine/core';
 import {
   IconLogout,
   IconHeart,
   IconStar,
-  IconMessage,
-  IconSettings,
+  IconFiles,
+  IconUser,
   IconPlayerPause,
   IconTrash,
   IconSwitchHorizontal,
   IconChevronDown,
 } from '@tabler/icons-react';
-import { MantineLogo } from '@mantinex/mantine-logo';
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 import classes from './UserMenu.module.css';
 
 const user = {
@@ -33,27 +22,10 @@ const user = {
   image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png',
 };
 
-const tabs = [
-  'Home',
-  'Orders',
-  'Education',
-  'Community',
-  'Forums',
-  'Support',
-  'Account',
-  'Helpdesk',
-];
-
 export function UserMenu() {
+  const { data: session } = useSession();
   const theme = useMantineTheme();
-  const [opened, { toggle }] = useDisclosure(false);
   const [userMenuOpened, setUserMenuOpened] = useState(false);
-
-  const items = tabs.map((tab) => (
-    <Tabs.Tab value={tab} key={tab}>
-      {tab}
-    </Tabs.Tab>
-  ));
 
   return (
     <Menu
@@ -67,81 +39,45 @@ export function UserMenu() {
       <Menu.Target>
         <UnstyledButton className={cx(classes.user, { [classes.userActive]: userMenuOpened })}>
           <Group gap={7}>
-            <Avatar src={user.image} alt={user.name} radius="xl" size={20} />
+            <Avatar
+              src={session?.user?.image}
+              alt={session?.user?.name || ''}
+              radius="xl"
+              size={20}
+            />
             <Text fw={500} size="sm" lh={1} mr={3}>
-              {user.name}
+              {session?.user?.name}
             </Text>
             <IconChevronDown style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
           </Group>
         </UnstyledButton>
       </Menu.Target>
       <Menu.Dropdown>
+      <Menu.Label>General</Menu.Label>
         <Menu.Item
           leftSection={
-            <IconHeart
-              style={{ width: rem(16), height: rem(16) }}
-              color={theme.colors.red[6]}
-              stroke={1.5}
-            />
-          }
-        >
-          Liked posts
-        </Menu.Item>
-        <Menu.Item
-          leftSection={
-            <IconStar
-              style={{ width: rem(16), height: rem(16) }}
-              color={theme.colors.yellow[6]}
-              stroke={1.5}
-            />
-          }
-        >
-          Saved posts
-        </Menu.Item>
-        <Menu.Item
-          leftSection={
-            <IconMessage
+            <IconFiles
               style={{ width: rem(16), height: rem(16) }}
               color={theme.colors.blue[6]}
               stroke={1.5}
             />
           }
         >
-          Your comments
+          My Documents
         </Menu.Item>
-
+        <Menu.Divider />
         <Menu.Label>Settings</Menu.Label>
         <Menu.Item
-          leftSection={<IconSettings style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
+          leftSection={<IconUser style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
         >
-          Account settings
+          Profile
         </Menu.Item>
+
         <Menu.Item
-          leftSection={
-            <IconSwitchHorizontal style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-          }
-        >
-          Change account
-        </Menu.Item>
-        <Menu.Item
+          onClick={() => signOut()}
           leftSection={<IconLogout style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
         >
           Logout
-        </Menu.Item>
-
-        <Menu.Divider />
-
-        <Menu.Label>Danger zone</Menu.Label>
-        <Menu.Item
-          leftSection={<IconPlayerPause style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
-        >
-          Pause subscription
-        </Menu.Item>
-        <Menu.Item
-          color="red"
-          leftSection={<IconTrash style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
-        >
-          Delete account
         </Menu.Item>
       </Menu.Dropdown>
     </Menu>
